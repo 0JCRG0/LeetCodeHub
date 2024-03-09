dd23125609fb8986b81dfcc5faa39437c91fc0bd
0JCRG0 <112776841+0JCRG0@users.noreply.github.com>
2024-03-09 03:39:41 +0000

    syntax
    syntax

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from patterns.SlidingWindow import SlidingWindowSolutions
 
 
 if __name__ == "__main__":
-    instance = SlidingWindowSolutions()
-    ans = instance.findMaxAverage()
+    sliding_window = SlidingWindowSolutions()
+    #ans = sliding_window.findMaxAverage()
+    ans = sliding_window.minSubArrayLen()
     print(ans)
--- patterns/SlidingWindow.py
+++ patterns/SlidingWindow.py
@@ -5,18 +5,18 @@ class SlidingWindowSolutions:
         """
         You are given an integer array nums consisting of n elements, and an integer k.
 
-        Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value.
+        Find a contiguous subarray whose length is equal to k that has the maximum average value and return             this value.
+        
         Any answer with a calculation error less than 10-5 will be accepted.
 
         Example 1:
-
-        Input: nums = [1,12,-5,-6,50,3], k = 4
-        Output: 12.75000
-        Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+            Input: nums = [1,12,-5,-6,50,3], k = 4
+            Output: 12.75000
+            Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+            
         Example 2:
-
-        Input: nums = [5], k = 1
-        Output: 5.00000
+            Input: nums = [5], k = 1
+            Output: 5.00000
         """
         
         n = len(nums)
@@ -40,19 +40,19 @@ class SlidingWindowSolutions:
         """
         Given an array of positive integers nums and a positive integer target, return the minimal length of a 
         subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
-        Example 1:
-        Input: target = 7, nums = [2,3,1,2,4,3]
-        Output: 2
-        Explanation: The subarray [4,3] has the minimal length under the problem constraint.
         
+        Example 1:
+            Input: target = 7, nums = [2,3,1,2,4,3]
+            Output: 2
+            Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+            
         Example 2:
-        Input: target = 4, nums = [1,4,4]
-        Output: 1
+            Input: target = 4, nums = [1,4,4]
+            Output: 1
         
         Example 3:
-        Input: target = 11, nums = [1,1,1,1,1,1,1,1]
-        Output: 0
-        
+            Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+            Output: 0
         """
 
         n = len(nums)
@@ -65,9 +65,9 @@ class SlidingWindowSolutions:
         for end in range(n):
             window_sum += nums[end]
             """
-            Esto va haciendo el subarray mas grande hasta que el sum sea mayor o igual a target
-            Window sum se hará más pequeño (al quitarle el primer elemento del subarray, esto se hará hasta que window_sum sea mayor a target)
-            Mientras que el len minimo ya se guardo, este se actualizara cada vez que window sum sea mayor a target.
+            Esto va haciendo el subarray mas grande hasta que el sum sea mayor o igual a target Window sum se               hará más pequeño (al quitarle el primer elemento del subarray, esto se hará hasta que window_sum                sea mayor a target)
+            
+            Mientras que el len minimo ya se guardo, este se actualizara cada vez que window sum sea mayor a                target.
             """
             #print(window_sum)
 
