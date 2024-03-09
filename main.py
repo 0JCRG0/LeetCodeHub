ecf03419f25e107638e8d14b730c952f69725f54
JCRG <juancarlosrg1999@gmail.com>
2024-03-08 22:05:50 -0600

    twoSum

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from patterns.SlidingWindow import SlidingWindowSolutions
+from data_structures.HashTables import HashTableSolutions
 
 
 
 if __name__ == "__main__":
     sliding_window = SlidingWindowSolutions()
+    hash_table = HashTableSolutions()
+
+    """
+    Sliding Window
+    """
     #ans = sliding_window.findMaxAverage()
-    ans = sliding_window.minSubArrayLen()
+    #ans = sliding_window.minSubArrayLen()
+    """
+    Hash Table
+    """
+    ans = HashTableSolutions.TwoSum()
+    
+    #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
     print(ans)
