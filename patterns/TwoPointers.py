7b5e6d9779338974077da5f8193c43e0760ed232
JCRG <juancarlosrg1999@gmail.com>
2024-03-13 21:55:44 -0600

    subsequence

--- patterns/TwoPointers.py
+++ patterns/TwoPointers.py
@@ -46,10 +46,43 @@ class TwoPointersSolutions():
             return final_str
         
         return compare(s) == compare(t)
+    def IsSubsequence_392(self, s:str = "aeb", t:str = "abcde"):
+        """
+        Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
 
+        A subsequence of a string is a new string that is formed from the original string by deleting 
+        some (can be none) of the characters without disturbing the relative positions of 
+        the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+        Example 1:
+        Input: s = "abc", t = "ahbgdc"
+        Output: true
+
+        Example 2:
+        Input: s = "axc", t = "ahbgdc"
+        Output: false
+
+        Constraints:
+        0 <= s.length <= 100
+        0 <= t.length <= 104
+        s and t consist only of lowercase English letters.
+        """
+        s_index, t_index = 0, 0
+
+        # Loop through both strings until we reach the end of one of them
+        while s_index < len(s) and t_index < len(t):
+            # If the characters match, move to the next character in s
+            if s[s_index] == t[t_index]:
+                print(s[s_index], t[t_index])
+                s_index += 1
+            # Always move to the next character in t
+            t_index += 1
+
+        # If we've gone through the entire s, then it's a subsequence
+        return s_index == len(s)
 
 if __name__ == "__main__":
     instance = TwoPointersSolutions()
-    backspace_ans = instance.backspaceCompare()
-    print(backspace_ans)
+    ans = instance.IsSubsequence_392()
+    print(ans)
 
